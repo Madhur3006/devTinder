2460c77212ad9ef4f18af3b94e7f929c0efdd1b7
Madhur Mangal <madhurmangal5@gmail.com>
2025-07-19 00:52:08 +0530

    making user model and operations

--- src/app.js
+++ src/app.js
@@ -2,6 +2,33 @@ const express = require("express");
 const {connectDB} = require('./config/database')
 const app = express();
 
+const User = require("./models/user")
+
+app.post("/signup", async (req, res) => {
+    const user = new User({
+        firstName: "Madhur",
+        lastName: "Mangal",
+        age: 25,
+        gender: "male"
+
+    },
+    {
+        firstName: "Madhur",
+        lastName: "Mangal",
+        age: 25,
+        gender: "male"
+
+    })
+    try {
+        await user.save()
+        res.send("data added successfully")
+    } catch (error) {
+        res.send(`error saving data ${error}`)
+    }
+})
+
+
+
 connectDB().then(() => {
     console.log("establising database connection")
     app.listen(3000, () => {
